ac2720a86ef5e1664e46e6392dd5e8e93bdf3ec6
Ollyfad <florencefadipe4@gmail.com>
2022-10-05 19:46:40 +0100

    fifth commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a39723010682141f49f499cce2271722-2141686155.us-east-1.elb.amazonaws.com'
 };
 
 /*
